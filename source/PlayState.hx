18daf73f79343ff99e505ab633023613aa900cb5
lucas <lucas.gangnant@epitech.com>
2024-03-29 11:37:09 +0400

    rewriting base of code for more flexibility

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,38 +10,23 @@ import openfl.display.Shape;
 
 class PlayState extends FlxState
 {
-	var array_card = new Array<FlxSprite>();
+	var array_card = new Array<Array<FlxSprite>>();
 
 	override public function create()
 	{
 		super.create();
+		var map = [1 => 50, 2 => 120, 3 => 190];
+		var y_pos = [50, 120, 190];
 
-		var map = [1 => 100, 2 => 150, 3 => 200];
 		for (i in 0...3) {
-			if (i == 0) {
-				for (key => value in map) {
-					var blueGraphic:FlxSprite = new FlxSprite(100, value);
-					blueGraphic.makeGraphic(30, 30, FlxColor.BLUE);
-					array_card.push(blueGraphic);
-					add(blueGraphic);
-				}
+			var array_line:Array<FlxSprite> = [];
+			for (key => value in map) {
+				var card:FlxSprite = new FlxSprite(value, y_pos[i], "assets/images/cat.png");
+				card.updateHitbox();
+				array_line.push(card);
+				add(card);
 			}
-			if (i == 1) {
-				for (key => value in map) {
-					var blueGraphic:FlxSprite = new FlxSprite(150, value);
-					blueGraphic.makeGraphic(30, 30, FlxColor.RED);
-					array_card.push(blueGraphic);
-					add(blueGraphic);
-				}
-			}
-			if (i == 2) {
-				for (key => value in map) {
-					var blueGraphic:FlxSprite = new FlxSprite(200, value);
-					blueGraphic.makeGraphic(30, 30, FlxColor.GREEN);
-					array_card.push(blueGraphic);
-					add(blueGraphic);
-				}
-			}
+			array_card.push(array_line);
 		}
 	}
 
@@ -49,13 +34,16 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 		
-		for (card in array_card) {
-			if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(card)) {
-				var new_color = new FlxSprite(card.x, card.y);
-				new_color.makeGraphic(30, 30, FlxColor.GREEN);
-				var index:Int = array_card.indexOf(card);
-				array_card[index] = new_color;
-				add(array_card[index]);
+		for (line in array_card) {
+			for (card in line) {
+				if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(card)) {
+					var new_color:FlxSprite = new FlxSprite(card.x, card.y, "assets/images/pog.png");
+					new_color.updateHitbox();
+					var index:Int = line.indexOf(card);
+					remove(line[index]);
+					line[index] = new_color;
+					add(line[index]);
+				}
 			}
 		}
 	}
