b0341c56ee27727a017b404729c0231453309209
lucas <lucas.gangnant@epitech.com>
2024-04-04 15:55:20 +0400

    switch view system added

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.addons.effects.chainable.FlxShakeEffect;
 import flixel.animation.FlxBaseAnimation;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -14,6 +15,9 @@ class Card extends FlxSprite {
 	public function new(?x:Float = 0, ?y:Float = 0, ?asset:String = "assets/images/cat.png") {
 		super(x, y);
 		loadGraphic(asset);
+		// this.scale.x = 0.85;
+		// this.scale.y = 0.85;
+		// this.updateHitbox();
 	}
 
 	override public function update(elapsed:Float):Void {
@@ -36,7 +40,9 @@ class PlayState extends FlxState
 	var solution_flip_state = new Array<Bool>();
 	var array_card = new Array<Array<Card>>();
 	var player_flip_state = new Array<Bool>();
-
+	var hide_button = new FlxSprite();
+	var soluce_view:Bool = true;
+	var current_view_txt = new FlxText();
 	var random = new flixel.math.FlxRandom();
 
 	override public function create()
@@ -44,6 +50,7 @@ class PlayState extends FlxState
 		super.create();
 
 		generate_puzzle_solution();
+		create_hide_soluce_button();
 		generate_puzzle_player();
 	}
 
@@ -52,16 +59,19 @@ class PlayState extends FlxState
 		super.update(elapsed);
 		var line_cursor:Int = 0;
 		
-		for (line in array_card) {
-			var index:Int = 0;
-			for (card in line) {
-				if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(card)) {
-					click_card(array_card, line, card, index, line_cursor);
+		if (soluce_view == false) {
+			for (line in array_card) {
+				var index:Int = 0;
+				for (card in line) {
+					if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(card)) {
+						click_card(array_card, line, card, index, line_cursor);
+					}
+					index++;
 				}
-				index++;
+				line_cursor++;
 			}
-			line_cursor++;
-		}
+		}	
+		switch_views();
 		check_resolved_puzzle();
 	}
 
@@ -135,9 +145,35 @@ class PlayState extends FlxState
 		}
 	}
 
+	public function create_hide_soluce_button() {
+		hide_button.makeGraphic(128, 64, FlxColor.RED);
+		hide_button.x = 200;
+		hide_button.y = 380;
+		add(hide_button);
+
+		var switch_button_text = new FlxText();
+		switch_button_text.text = "Switch";
+		switch_button_text.size = 24;
+		switch_button_text.x = hide_button.x + 10;
+		switch_button_text.y = hide_button.y + 10;
+		add(switch_button_text);
+
+		current_view_txt.text = "Solution";
+		current_view_txt.size = 24;
+		current_view_txt.x = 100;
+		current_view_txt.y = 30;
+		add(current_view_txt);
+	}
+
 	public function generate_puzzle_solution() {
-		var pos_x = [70, 134, 198];
-		var pos_y = [30, 94, 158];
+		var spacing = 64;
+		var pos_x = [30];
+		var pos_y = [110];
+
+		for (i in 0...3)
+			pos_x.push(pos_x[pos_x.length - 1] + 64);
+		for (i in 0...3)
+			pos_y.push(pos_y[pos_y.length - 1] + 64);
 
 		for (y in pos_y) {
 			var array_line:Array<Card> = [];
@@ -150,13 +186,14 @@ class PlayState extends FlxState
 			array_card_solution.push(array_line);
 		}
 
+		// choose random card to reverse
 		for (i in 0...random.int(2, 6)) {
 			var rand_row = random.int(0, array_card_solution.length - 1);
 			var rand_index = random.int(0, array_card_solution[rand_row].length - 1);
 
-			// trace(rand_row);
-			// trace(rand_index);
-			// trace("========");
+			trace(rand_row);
+			trace(rand_index);
+			trace("========");
 
 			if (array_card_solution[rand_row][rand_index].flipped == true)
 				array_card_solution[rand_row][rand_index].flipped = false;
@@ -166,27 +203,63 @@ class PlayState extends FlxState
 				rand_index, rand_row);
 		}
 		set_solution_flip_state();
-		trace(solution_flip_state);
-		trace("-------------");
 	}
 
 	public function generate_puzzle_player() {
-		var pos_x = [70, 134, 198];
-		var pos_y = [250, 314, 378];
+		var spacing = 64;
+		var pos_x = [30];
+		var pos_y = [110];
+
+		for (i in 0...3)
+			pos_x.push(pos_x[pos_x.length - 1] + 64);
+		for (i in 0...3)
+			pos_y.push(pos_y[pos_y.length - 1] + 64);
+
 
 		for (y in pos_y) {
 			var array_line:Array<Card> = [];
 			for (x in pos_x) {
 				var card:Card = new Card(x, y);
 				card.updateHitbox();
+				card.visible = false;
 				array_line.push(card);
 				add(card);
 			}
 			array_card.push(array_line);
 		}
 		update_flip_state();
-		trace(player_flip_state);
-		trace("-------------");
+	}
+
+	public function switch_views() {
+		if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(hide_button)) {
+			if (soluce_view == true) {
+				soluce_view = false;
+				current_view_txt.text = "Player";
+				for (line in array_card_solution) {
+					for (solution_card in line) {
+						solution_card.visible = false;
+					}
+				}
+				for (line in array_card) {
+					for (player_card in line) {
+						player_card.visible = true;
+					}
+				}
+			} else {
+				soluce_view = true;
+				current_view_txt.text = "Solution";
+				for (line in array_card_solution) {
+					for (solution_card in line) {
+						solution_card.visible = true;
+					}
+				}
+				for (line in array_card) {
+					for (player_card in line) {
+						player_card.visible = false;
+					}
+				}
+			}
+		}
 	}
 
 	public function check_resolved_puzzle() {
@@ -198,12 +271,12 @@ class PlayState extends FlxState
 			else
 				matching_card_state++;
 		}
-		// if (matching_card_state == solution_flip_state.length) {
-		// 	// trace("SOLVED !");
-		// 	FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
-		// 	{
-		// 		FlxG.switchState(new MenuState());
-		// 	});
-		// }
+		if (matching_card_state == solution_flip_state.length) {
+			// trace("SOLVED !");
+			FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
+			{
+				FlxG.switchState(new MenuState());
+			});
+		}
 	}
 }
